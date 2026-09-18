b30edd5056249307a8d7754cc4d1f7a12d2af738
Raika Zolfagharimanesh <ryka8@yahoo.com>
2026-09-17 23:38:24 -0500

    assignment2
    Updated author name and student ID in comments.

---
+++ 3215927_Zolfagharimanesh_Assignment_2/assignment2.c
@@ -0,0 +1,23 @@
+/*
+Program: EECS 348 Assignment 2
+
+Description:
+This program prioritizes emails for a CEO using a MaxHeap.
+Emails are prioritized by sender category and then by date.
+
+Input:
+EMAIL, NEXT, READ, and COUNT commands from a test file.
+
+Output:
+Displays the next email and the number of unread emails.
+
+Collaborators: None
+Other Sources: Grok and Microsoft Copilot
+Author: Raika Zolfagharimanesh
+Student ID: 3215927
+Creation Date: September 17, 2026
+Revision Date: September 17, 2026
+
+Revisions: None
+*/
+
